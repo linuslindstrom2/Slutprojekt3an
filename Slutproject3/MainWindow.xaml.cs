700042945b5eac3efd19c15b8d75cdaaccf12668
linuslindstrom2 <54801743+linuslindstrom2@users.noreply.github.com>
2021-05-16 21:52:20 +0200

    kan gå nu

--- Slutproject3/MainWindow.xaml.cs
+++ Slutproject3/MainWindow.xaml.cs
@@ -18,34 +18,49 @@ namespace Slutproject3
 {
     public partial class MainWindow : Window 
     {
-       
         List<Ellipse> Positions = new List<Ellipse>();
         List<Ellipse> RedGoalPath = new List<Ellipse>(); 
         List<Ellipse> GreenGoalPath = new List<Ellipse>(); 
         List<Ellipse> YellowGoalPath = new List<Ellipse>();
         List<Ellipse> BlueGoalPath = new List<Ellipse>();
 
+        List<Image> DiceSides = new List<Image>();
+
         ImageBrush redPlayerImage = new ImageBrush();
         ImageBrush bluePlayerImage = new ImageBrush();
-       
+        int totalPosition = 0; 
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void SetupGame()
+        public void SetupGame()
         {
             redPlayerImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/RödPjäs.png"));
+            bluePlayerImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/BlåPjäs.png"));
 
-            Rectangle redPlayer = new Rectangle
+            Rectangle redPlayer = new Rectangle()
             {
                 Height = 80,
                 Width = 140,
-                Fill = redPlayerImage, 
+                Fill = redPlayerImage,
+                Name = "RedPlayer"
             };
+            Grid.SetColumn(redPlayer, 10);
+            Grid.SetRow(redPlayer, 2);
             MyGrid.Children.Add(redPlayer);
-
-            MovePlayer(redPlayer, 41);
+            
+            Rectangle bluePlayer = new Rectangle()
+            {
+                Height = 80,
+                Width = 140,
+                Fill = bluePlayerImage,
+                Name = "BluePlayer"
+            };
+            Grid.SetColumn(bluePlayer, 2);
+            Grid.SetRow(bluePlayer, 2);
+            MyGrid.Children.Add(bluePlayer);
         }
 
         private void MovePlayer(Rectangle player, int g)
@@ -53,15 +68,67 @@ namespace Slutproject3
             if (g >= 40)
             {
                 g -= 40;
-                Grid.SetColumn(player, Grid.GetColumn(RedGoalPath[g]));
-                Grid.SetRow(player, Grid.GetRow(RedGoalPath[g]));
+                if (g >= 5)
+                {
+                    g = 4;
+                    foreach (var element in MyGrid.Children)
+                    {
+                        if (element is TextBox Winning)
+                        {
+                            if (Winning.Name == "winning")
+                            {
+                                Winning.Text += player.Name + " won!!";
+                            }
+                        }
+                    }
+                }
+               
+                
+                if (player.Name.StartsWith("Red"))
+                {
+                    Grid.SetColumn(player, Grid.GetColumn(RedGoalPath[g]));
+                    Grid.SetRow(player, Grid.GetRow(RedGoalPath[g]));
+                }
+                else if (player.Name.StartsWith("Green"))
+                {
+                    Grid.SetColumn(player, Grid.GetColumn(GreenGoalPath[g]));
+                    Grid.SetRow(player, Grid.GetRow(GreenGoalPath[g]));
+                }
+                else if (player.Name.StartsWith("Yellow"))
+                {
+                    Grid.SetColumn(player, Grid.GetColumn(YellowGoalPath[g]));
+                    Grid.SetRow(player, Grid.GetRow(YellowGoalPath[g]));
+                }
+                else if (player.Name.StartsWith("Blue"))
+                {
+                    Grid.SetColumn(player, Grid.GetColumn(BlueGoalPath[g]));
+                    Grid.SetRow(player, Grid.GetRow(BlueGoalPath[g]));
+                }
             }
             else
             {
                 Grid.SetColumn(player, Grid.GetColumn(Positions[g]));
                 Grid.SetRow(player, Grid.GetRow(Positions[g]));
             }
+        }
 
+        private void RollDice(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            int diceNumber = random.Next(1, 7);
+
+            totalPosition += diceNumber;
+
+            foreach (var element in MyGrid.Children)
+            {
+                if (element is Rectangle rect)
+                {
+                    if (rect.Name== "RedPlayer")
+                    {
+                        MovePlayer(rect, totalPosition);
+                    }
+                }
+            }
         }
 
         public void AddCircleToList(Brush fill, Brush stroke, int radius, int i, int row, int collumn, string färg)
@@ -77,17 +144,27 @@ namespace Slutproject3
             Grid.SetColumn(rec, collumn);
             MyGrid.Children.Add(rec);
             rec.Name = färg + i.ToString();
-            if (rec.Name.StartsWith("t"))
+
+            if (rec.Name.StartsWith("RedGoal"))
             {
                 RedGoalPath.Add(rec);
             }
-            
-            else 
+            else if (rec.Name.StartsWith("GreenGoal"))
             {
-                
-                Positions.Add(rec);
+                GreenGoalPath.Add(rec);
             }
-                       
+            else if (rec.Name.StartsWith("YellowGoal"))
+            {
+                YellowGoalPath.Add(rec);
+            }
+            else if (rec.Name.StartsWith("BlueGoal"))
+            {
+                BlueGoalPath.Add(rec);
+            }
+            else
+            {
+                Positions.Add(rec);
+            }          
         }
 
         public void AddCircleToGrid(Brush fill, Brush stroke, int radius, int i, int row, int collumn, string färg)
@@ -103,13 +180,11 @@ namespace Slutproject3
             Grid.SetColumn(rec, collumn);
             MyGrid.Children.Add(rec);
             rec.Name = färg + i.ToString();
-           
         }
 
         public void MyGrid_Loaded(object sender, EventArgs e)
         {
-            
-            for (int i = 0; i < 13; i++)
+            for (int i = 0; i < 20; i++)
             {
                 var row = new RowDefinition();
                 row.Height = new GridLength(60);
@@ -118,8 +193,6 @@ namespace Slutproject3
                 col.Width = new GridLength(60);
                 MyGrid.ColumnDefinitions.Add(col);
             }
-
-            TextBox Text = new TextBox();
 
             for (int j = 1; j < 6; j++)
             {
@@ -185,31 +258,31 @@ namespace Slutproject3
 
             AddCircleToList(Brushes.White, Brushes.Black, 58, 10, 1, 6, "BluePath");
 
-            for (int j = 2; j < 6; j++)
+            for (int j = 2; j < 7; j++)
             {
                 int i = 1;
-                AddCircleToList(Brushes.Red, Brushes.Black, 58, i, j, 6, "tGoalPath"); // fixa rätt lista på dessa så de inte ligger i red
+                AddCircleToList(Brushes.Red, Brushes.Black, 58, i, j, 6, "RedGoalPath"); 
+                i += 1;
+            }
+
+            for (int j = 10; j > 6; j--)
+            {
+                int i = 4;
+                AddCircleToList(Brushes.Green, Brushes.Black, 58, i, 6, j, "GreenGoalPath");
                 i += 1;
             }
 
             for (int j = 7; j < 11; j++)
             {
                 int i = 4;
-                AddCircleToList(Brushes.Green, Brushes.Black, 58, i, 6, j, "tGoalPath");
-                i += 1;
-            }
-
-            for (int j = 7; j < 11; j++)
-            {
-                int i = 4;
-                AddCircleToList(Brushes.Yellow, Brushes.Black, 58, i, j, 6, "tGoalPath");
+                AddCircleToList(Brushes.Yellow, Brushes.Black, 58, i, j, 6, "YellowGoalPath");
                 i += 1;
             }
 
             for (int j = 2; j < 6; j++)
             {
                 int i = 1;
-                AddCircleToList(Brushes.Blue, Brushes.Black, 58, i, 6, j, "tGoalPath");
+                AddCircleToList(Brushes.Blue, Brushes.Black, 58, i, 6, j, "BlueGoalPath");
                 i += 1;
             }
 
@@ -221,7 +294,6 @@ namespace Slutproject3
 
             AddCircleToGrid(Brushes.Yellow, Brushes.Black, 58, 4, 11, 5, "EndastDesign");
 
-            AddCircleToGrid(Brushes.Purple, Brushes.Black, 58, 1, 6, 6, "Goal");
 
             Ellipse BlueStart = new Ellipse()
             {
@@ -274,7 +346,37 @@ namespace Slutproject3
             MyGrid.Children.Add(YellowStart);
             Grid.SetColumnSpan(YellowStart, 3);
             Grid.SetRowSpan(YellowStart, 3);
-            
+
+            Button Dice = new Button()
+            {
+                Content = "Dice",
+                Width = 100,
+                Height = 100,
+                Background = Brushes.Gray,
+                BorderBrush = Brushes.Black, 
+                
+            };
+            Grid.SetRow(Dice, 3);
+            Grid.SetColumn(Dice, 14);
+            Grid.SetColumnSpan(Dice, 2);
+            Grid.SetRowSpan(Dice, 2);
+            MyGrid.Children.Add(Dice);
+            Dice.Click += RollDice;
+
+            TextBox Winning = new TextBox
+            {
+                Height = 50,
+                Width = 100,
+                Background = Brushes.White,
+                BorderBrush = Brushes.Black,
+                Name = "winning", 
+            };
+            Grid.SetColumn(Winning, 14);
+            Grid.SetRow(Winning, 1);
+            Grid.SetColumnSpan(Winning, 2);
+
+            MyGrid.Children.Add(Winning);
+
             SetupGame();
         }
     }
